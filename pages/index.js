bec409c9354d3781ae02934779ba4783edbe89a1
camerondrennan <cameron.david.george.drennan@gmail.com>
2018-10-14 15:14:51 +0100

    Imported app bar from material ui to ensure the configuration is working.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Star from '@material-ui/icons/Star';
+import AppBar from '../components/AppBar';
 
 export default (props) => (
     <div>
+        <AppBar />
         <div>Hello World</div>
         <Grid item xs={8}>
             <Star className={props.icon} color="primary"/>
